5091d8ca98ec2e5531ddf6d482da88828c2516df
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-05-19 15:08:30 -0400

    Fixed nav links. Moving on to contact section.

--- src/sections/navbar/links.js
+++ src/sections/navbar/links.js
@@ -2,7 +2,7 @@ const links = [
   { id: 1, link: "#", label: "Home" },
   { id: 2, link: "#about", label: "About" },
   { id: 3, link: "#portfolio", label: "Portfolio" },
-  { id: 4, link: "#current-learning", label: "Current Learning" },
+  { id: 4, link: "#experience", label: "Experience" },
   // { id: 5, link: "#faqs", label: "FAQs" },
   // { id: 6, link: "#services", label: "Services" },
   { id: 7, link: "#contact", label: "Contact" }
