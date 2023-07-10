c9c082842784e21d80686e35dc2a51f8496d0844
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-07-10 14:32:15 -0400

    Added Pokedex project.

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -1,4 +1,4 @@
-import Project1 from "../../assets/project1.jpg"
+import Pokedex from "../../assets/Pokedex.png"
 import Project2 from "../../assets/project2.jpg"
 import Project3 from "../../assets/project3.jpg"
 import Project4 from "../../assets/project4.jpg"
@@ -8,11 +8,11 @@ const data = [
   {
     id: 1,
     category: "Frontend",
-    image: Project1,
-    title: "Project One (FE)",
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    demo: "https://www.google.com",
-    github: "github.url"
+    image: Pokedex,
+    title: "Pokedex (FE)",
+    desc: "I suppose no portfolio is complete without a project utilizing the pokeAPI. While this project is quite simple, I tried to showcase my ability to sort data from an API, and allow the user to search through that data. This project also contains custom bar charts for comparison using only css styling.",
+    demo: "https://sballay8734.github.io/pokedex/",
+    github: "https://github.com/sballay8734/pokedex"
   },
   {
     id: 2,
